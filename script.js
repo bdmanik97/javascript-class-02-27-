d8a0af55803fca2a20c67db66365120f8f87a399
bdmanik97 <inmanik97@gmail.com>
2024-01-15 15:07:59 +0600

    {class-02}

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// index arrayy
+let marks = [55,66,88,99,77,22,33,44,11,102,502,555,'manik_kumar_ray']
+
+console.log(marks[2]);
+
+// two diamensional array
+let twoDimensionalArray = [
+    ['Rohim', 'Korim', 'Bablu', 'Manik', 'Ratul'],
+    ['Biploby', 'Susmita', 'Moni', 'Mily'],
+    ['Manikul', 'Nasir', 'Mamun', 'Prince', 'Avijit'],
+    ['Dony', 'Kamona', 'Bonni'],
+]
+
+console.log (twoDimensionalArray[2][4]);
+
+// Associative array
+
+let manik = {
+    firstName: 'manik', lastName: 'ray', age: '28', accountBalance: '55454.2021'
+}
+console.log(manik.lastName)
